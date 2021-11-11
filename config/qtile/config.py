c0fa8bcd271589604dba2be296940cc6e9dadb5c
Ashutosh Sathe <2ashutoshbs@gmail.com>
2021-11-11 16:50:14 +0530

    Switch to super key as modkey like normal people
    I'm trying to blend in guys

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -34,7 +34,7 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-mod = "mod1"
+mod = "mod4"
 # terminal = guess_terminal()
 my_term = "alacritty"
 my_browsers = ["firefox", "google-chrome-stable -incognito"]
