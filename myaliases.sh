349adf16624e99bdceffaa24cd29c4976942710c
Ashutosh Sathe <2ashutoshbs@gmail.com>
2022-08-11 11:55:12 +0530

    New bash function

--- myaliases.sh
+++ myaliases.sh
@@ -11,3 +11,23 @@ powner() {
 
 # NVIDIA
 alias nvinfnoheader="nvidia-smi --query-gpu=fan.speed,temperature.gpu,pstate --format=csv,noheader"
+
+# General bash stuff 
+logtofile() {
+    passed_cmd="$*"
+    fname="$1"
+    timestamp=$(date +%F.%H-%M-%S)
+    echo "Executing: $passed_cmd"
+    echo "Logging to: $fname.$timestamp.log"
+    echo "Is the command escaped properly? [y/N]" 
+    read escape_response
+    if [[ $escape_response =~ ^[Yy]$ ]]
+    then
+        echo "-----"
+        eval "$passed_cmd" 2>&1 | tee $fname.$timestamp.log 
+        echo -e "\n\n\n-----\nCommand: $passed_cmd\n-----" >> $fname.$timestamp.log
+        echo "-----"
+    else
+        echo "Exiting"
+    fi
+}
