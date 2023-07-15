a39e003c843c081b2a67a5761191ccc7fa5aa542
Ashutosh Sathe <2ashutoshbs@gmail.com>
2023-07-15 16:48:52 +0530

    spicy new plugins

--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -33,17 +33,51 @@ require('lazy').setup({
     {
         'ms-jpq/coq_nvim',
         branch = 'coq',
+        build = ':COQDeps',
     },
     {
         'ms-jpq/coq.artifacts',
         branch = 'artifacts',
+    }, -- Can also potentially use https://github.com/ms-jpq/coq.thirdparty
+    'ryanoasis/vim-devicons',
+    'preservim/nerdtree',
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    -- Can also potentially use https://github.com/ms-jpq/coq.thirdparty
+    'NvChad/nvim-colorizer.lua',
+    'sunjon/shade.nvim',
+    'navarasu/onedark.nvim',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = {
+            ensure_installed = {
+                'c',
+                'cpp',
+                'cuda',
+                'dot',
+                'lua',
+                'vim',
+                'python',
+                'bash',
+                'git_config',
+                'git_rebase',
+                'gitattributes',
+                'gitcommit',
+                'gitignore',
+                'glsl',
+            }
+        }
+    },
     'folke/which-key.nvim',
 })
 
+-- Mason
 require("mason").setup()
 
+-- Language server + coq
 local lsp = require('lspconfig')
 local coq = require('coq')
 lsp.pyright.setup(coq.lsp_ensure_capabilities({}))
@@ -51,3 +85,62 @@ lsp.clangd.setup(coq.lsp_ensure_capabilities({}))
 lsp.ltex.setup(coq.lsp_ensure_capabilities({}))
 lsp.awk_ls.setup(coq.lsp_ensure_capabilities({}))
 lsp.bashls.setup(coq.lsp_ensure_capabilities({}))
+
+-- NERDTree
+vim.keymap.set('n', '<leader>n', ':NERDTreeFocus<CR>')
+vim.keymap.set('n', '<C-n>', ':NERDTree<CR>')
+vim.keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>')
+vim.keymap.set('n', '<C-f>', ':NERDTreeFind<CR>')
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Colorize
+-- TODO: Is this really correct?
+require('colorizer').setup()
+
+-- Shade
+require('shade').setup({
+    overlay_opacity = 50,
+    opacity_step = 1,
+    keys = {
+        brightness_up = '<C-Up>',
+        brightness_down = '<C-Down>',
+        toggle = '<leader>s',
+    }
+})
+
+-- Onedark
+require('onedark').setup({
+    style = 'darker'
+})
+require('onedark').load()
+
+-- Treesitter
+require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+        'c',
+        'cpp',
+        'cuda',
+        'dot',
+        'lua',
+        'vim',
+        'python',
+        'bash',
+        'git_config',
+        'git_rebase',
+        'gitattributes',
+        'gitcommit',
+        'gitignore',
+        'glsl',
+    },
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    }
+})
