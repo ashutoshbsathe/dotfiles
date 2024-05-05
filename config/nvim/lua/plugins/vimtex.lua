e7d11382f7295ecb8834d4ec719d1504bdd44413
Ashutosh Sathe <2ashutoshbs@gmail.com>
2024-05-05 15:03:26 +0530

    Use Zathura for PDF viewing

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_view_method = "zathura"
     end,
   },
   {
