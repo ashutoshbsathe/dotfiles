7d565f75538022da7c0e76b4e660f69461c7cc48
Ashutosh Sathe <2ashutoshbs@gmail.com>
2024-05-05 11:55:09 +0530

    Switch to LazyVim

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,39 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 16
+
+vim.opt.hlsearch = true
+
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+
+vim.opt.timeoutlen = 300 -- Lower than default (1000) to quickly trigger which-key
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
